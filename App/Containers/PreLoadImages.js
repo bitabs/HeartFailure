e140e889aa6a51e618f4b4ae68116c4a1430ef9a
Naseebullah Ahmadi <javastastic@gmail.com>
2018-04-14 17:03:04 +0100

    re added ECG and heart sound

--- App/Containers/PreLoadImages.js
+++ App/Containers/PreLoadImages.js
@@ -8,7 +8,12 @@ const Images = {
   "SXMrXfBvexQUXfnVg5WWVwsKjpD2"  : require('../Images/Levi.jpg'),
   "VoxHFgUEIwRFWg7JTKNXSSoFoMV2"  : require('../Images/Ernest.png'),
   "3gOlvmX4WoP3734IdtoeVCzpEy22"  : require('../Images/Haaris.png'),
-  "hFoWuyxv6Vbt8sEKA87T0720tXV2"  : require('../Images/Karla.jpg')
+  "hFoWuyxv6Vbt8sEKA87T0720tXV2"  : require('../Images/Karla.jpg'),
+  "viQIK6fxuIPlBbrm6yvSEkgXrmV2"  : require('../Images/Haaris.jpg'),
+  "ljgf1OIX4EOraaBfDfF0nAuEsA23"  : require('../Images/Khalil.jpg'),
+  "Hg6N1S4UVrh1HpDxCVMwb6NM7SF2"  : require('../Images/Mohammed.jpg'),
+  "DccDGBABo3PhDmfBmE871caEj9z1"  : require('../Images/Johnson.jpg')
+
 
 };
 
